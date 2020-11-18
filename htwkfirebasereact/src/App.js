415eb01b993e5a761e23599adb3931eb5c714414
Jonathan Wagner <jonathan.wagner@stud.htwk-leipzig.de>
2020-11-18 14:42:25 +0100

    todo app

--- htwkfirebasereact/src/App.js
+++ htwkfirebasereact/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Todo} from "./Todo";
+import {TodoForm} from "./TodoForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [todos, setTodos] = React.useState([
+        {
+            text: "Learn about React",
+            isCompleted: false
+        },
+        {
+            text: "Meet friend for lunch",
+            isCompleted: false
+        },
+        {
+            text: "Build really cool todo app",
+            isCompleted: false
+        }
+    ]);
+
+    const addTodo = text => {
+        const newTodos = [...todos, { text }];
+        setTodos(newTodos);
+    };
+
+    const completeTodo = index => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = !newTodos[index].isCompleted;
+        setTodos(newTodos);
+    };
+
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                        removeTodo={removeTodo}
+                    />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ htwkfirebasereact/src/TodoForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export const TodoForm = ({ addTodo }) => {
+    const [value, setValue] = React.useState("");
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        addTodo(value);
+        setValue("");
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className="input"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+            <button type={"submit"}>Sumbit</button>
+        </form>
+    );
+}
