17be6b8bce7b50071815a3f8d7d8f5705b7e27b1
Jonathan Wagner <jonathan.wagner@stud.htwk-leipzig.de>
2020-12-15 16:17:18 +0100

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,6 @@ import React from "react";
 import "./App.css";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
-import firebase from "firebase";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCMoVHAlfLjCZqBxSvsVWM4NEY25Rn0nLA",
-  authDomain: "htwkfirebasetodo.firebaseapp.com",
-  databaseURL: "https://htwkfirebasetodo.firebaseio.com",
-  projectId: "htwkfirebasetodo",
-  storageBucket: "htwkfirebasetodo.appspot.com",
-  messagingSenderId: "773853444894",
-  appId: "1:773853444894:web:f1e2ec3cade4f67bbee70f",
-};
-
-// Hallo My
-
-firebase.initializeApp(firebaseConfig);
 
 const App = () => {
   const [todos, setTodos] = React.useState([
